fe911fcc5c2e6eeccb6347abe18bf6f56dce52e2
Anastasiia Chepka <anastasiiac@wix.com>
2020-10-26 22:05:18 +0200

    Add top-down dp solution with memoization

--- equal-subset-sum-partition.js
+++ equal-subset-sum-partition.js
@@ -59,3 +59,45 @@ const canPartition = function(nums) {
 console.log(canPartition([1, 2, 3, 4]) === true);
 console.log(canPartition([1, 1, 3, 4, 7]) === true);
 console.log(canPartition([2, 3, 4, 6]) === false);
+
+/**
+ * @param {Array<number>} nums
+ * @returns {boolean}
+ */
+const canPartitionWithMemoization = function(nums) {
+    const sum = nums.reduce((acc, cur) => acc + cur, 0);
+
+    if (sum % 2 !== 0) {
+      return false;
+    }
+  
+    const dp = [];
+
+    function canPartionRecursive(nums, sum, curIndex) {
+        if (sum === 0) {
+            return true;
+        }
+    
+        if (curIndex >= nums.length || sum < 0) {
+            return false;
+        }
+
+        dp[curIndex] = dp[curIndex] || [];
+
+        if (dp[curIndex][sum] === undefined) {        
+            if (canPartionRecursive(nums, sum - nums[curIndex], curIndex + 1)) {
+                dp[curIndex][sum] = true;
+                return true;
+            }
+            
+            dp[curIndex][sum] = canPartionRecursive(nums, sum, curIndex + 1);
+            return dp[curIndex][sum];
+        }
+    }
+  
+    return canPartionRecursive(nums, sum/2, 0);
+};
+
+console.log(canPartitionWithMemoization([1, 2, 3, 4]) === true);
+console.log(canPartitionWithMemoization([1, 1, 3, 4, 7]) === true);
+console.log(canPartitionWithMemoization([2, 3, 4, 6]) === false);
